f80d6d0624b280638ea9b16c2fbfd39e7605d50b
yFrostyBr <148999260+yFrostyBr@users.noreply.github.com>
2023-10-27 09:35:35 -0300

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+# adj = input("Adjective: ")
+# verb1 = input("Verb: ")
+# verb2 = input("Verb: ")
+# famous_person = input("Famous Person: ")
+
+
+# madlib = f"Computer proggraming is so {adj} ! It make me so exited all the time because i\
+#     I love to {verb1}. Stay Hydrated and {verb2} like you are {famous_person}"
+
+# # print(madlib)
+
+
+Funcionário = input("Nome do Funcionário: ")
+Horário = input("Horário: ")
+Motivo = input("Motivo da Reclamação: ")
+
+teste = f"O funcionário {Funcionário}, ás {Horário}, registrou uma reclamação com o motivo:{Motivo} com sucesso."
+
+print(teste)
+
